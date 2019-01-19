6211d7b0c5c53a17a6b6eac59152e9ad7fc6c393
Alexey Shekhirin <a.shekhirin@gmail.com>
2019-01-19 21:42:51 +0300

    Run not debug by default

--- run.sh
+++ run.sh
@@ -14,7 +14,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 export DJANGO_SETTINGS_MODULE=goalboard.settings
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
-export DEBUG=FALSE
 
 
 $DJANGODIR/venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
