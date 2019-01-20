81ab67674450771320fcbff5f3ff83cc16aed85f
Alexey Shekhirin <a.shekhirin@gmail.com>
2019-01-20 19:15:08 +0300

    Add custom 404 (lol very simple)

--- goalboard_app/urls.py
+++ goalboard_app/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from goalboard_app import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('spreadsheet', views.user_spreadsheet, name='user_spreadsheet'),
     path('profile', views.user_profile, name='user_profile'),
     path('@<str:username>/', views.user_goalboard, name='user_goalboard')
 ]
+
+handler404 = 'goalboard_app.views.handler404'
