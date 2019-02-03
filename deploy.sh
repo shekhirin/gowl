92e40367898875359dc7488cd37c2c9b233d66bf
Alexey Shekhirin <a.shekhirin@gmail.com>
2019-02-03 19:48:21 +0300

    Remove shit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 cd /root/gowl
-git stash; git fetch origin master; git pull --rebase origin master;
+git clean -f; git stash; git fetch origin master; git pull --rebase origin master;
 bash run.sh $1
